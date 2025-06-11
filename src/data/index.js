5ec67272b6147a61c31e6c7b1f9f9cad9c350495
Francisco González Poveda <francisco.gonzalez@sax.es>
2025-06-11 13:50:07 +0200

    cambios en las descripciones de proyectos

--- src/data/index.js
+++ src/data/index.js
@@ -29,11 +29,11 @@ export const skillCards = [
   title: 'Front-End Dev',
   description: `Tengo experiencia sólida en desarrollo front-end utilizando HTML, CSS y JavaScript. Además, he trabajado con frameworks modernos como Angular y React para construir interfaces dinámicas, responsivas y orientadas a la experiencia del usuario. Me enfoco en rendimiento, accesibilidad y buenas prácticas de desarrollo.
 
-1. Ineffabile Pizza: Proyecto de fin de grado (TFG) en el que se desarrolló una web de pedidos online para una pizzería ficticia. Utilizamos HTML, CSS y JavaScript para el frontend y Angular para la zona de administración (back office).
+1. Ineffabile Pizza: Proyecto de fin de grado (TFG) en el que se desarrolló una web de pedidos online para una pizzería ficticia. Utilizamos HTML, CSS y JavaScript para el frontend, Angular para el back office y Node.js como backend principal. Además, se empleó Docker para virtualizar Elasticsearch y crear un sistema de búsqueda avanzado dentro de la web.
 
 2. GTA VI Characters: Aplicación realizada con Next.js como parte de una exploración técnica personal. El objetivo era experimentar con scroll suave utilizando Lenis y animaciones fluidas con Framer Motion, basándome en la temática de personajes del videojuego.
 
-3. Sax APP: Aplicación en desarrollo destinada al Ayuntamiento de Sax y sus ciudadanos. El objetivo es centralizar información municipal, eventos y servicios para facilitar la comunicación con los residentes.`,
+3. Sax APP: Aplicación en desarrollo destinada al Ayuntamiento de Sax y sus ciudadanos. Su objetivo es centralizar información municipal, eventos y servicios. Se ha desarrollado con React y Vite para el frontend, empleando Tailwind CSS para el diseño. El backend está construido con Node.js, incluyendo múltiples APIs para gestionar la comunicación con la base de datos.`,
   projectCount: 3,
   projectLinks: [
       'https://ineffabile-pizza.vercel.app/',
@@ -49,7 +49,11 @@ export const skillCards = [
   icon: 'bx bx-server',
   title: 'Back-End Dev',
   description:
-    `Como desarrollador back-end, tengo amplios conocimientos en Node.js y JavaScript. He trabajado en la creación de APIs, gestión de autenticaciones, procesos asincrónicos y lógica de negocio para aplicaciones escalables y seguras. Me preocupo por la eficiencia del código y la integración fluida con el front-end.`,
+    `Como desarrollador back-end, tengo amplios conocimientos en Node.js y JavaScript. He trabajado en la creación de APIs, gestión de autenticaciones, procesos asincrónicos y lógica de negocio para aplicaciones escalables y seguras. Me preocupo por la eficiencia del código y la integración fluida con el front-end.
+    
+    1. Ineffabile Pizza: Proyecto de fin de grado (TFG) en el que se desarrolló una web de pedidos online para una pizzería ficticia. Utilizamos HTML, CSS y JavaScript para el frontend, Angular para el back office y Node.js como backend principal. Además, se empleó Docker para virtualizar Elasticsearch y crear un sistema de búsqueda avanzado dentro de la web.
+    
+    2. Sax APP: Aplicación en desarrollo destinada al Ayuntamiento de Sax y sus ciudadanos. Su objetivo es centralizar información municipal, eventos y servicios. Se ha desarrollado con React y Vite para el frontend, empleando Tailwind CSS para el diseño. El backend está construido con Node.js, incluyendo múltiples APIs para gestionar la comunicación con la base de datos.`,
   projectCount: 2,
   projectLinks: [
       'https://ineffabile-pizza.vercel.app/',
@@ -64,7 +68,11 @@ export const skillCards = [
   icon: 'bx bx-data',
   title: 'Bases de Datos',
   description:
-    'Domino tanto bases de datos relacionales (MySQL, PostgreSQL) como no relacionales (MongoDB). Tengo experiencia en diseño de esquemas eficientes, optimización de consultas y gestión de grandes volúmenes de datos, asegurando integridad y rendimiento en las aplicaciones que desarrollo.',
+    `Domino tanto bases de datos relacionales (MySQL, PostgreSQL) como no relacionales (MongoDB). Tengo experiencia en diseño de esquemas eficientes, optimización de consultas y gestión de grandes volúmenes de datos, asegurando integridad y rendimiento en las aplicaciones que desarrollo.
+    
+    1. Ineffabile Pizza: Proyecto de fin de grado (TFG) en el que se desarrolló una web de pedidos online para una pizzería ficticia. Utilizamos HTML, CSS y JavaScript para el frontend, Angular para el back office y Node.js como backend principal. Además, se empleó Docker para virtualizar Elasticsearch y crear un sistema de búsqueda avanzado dentro de la web.
+    
+    2. Sax APP: Aplicación en desarrollo destinada al Ayuntamiento de Sax y sus ciudadanos. Su objetivo es centralizar información municipal, eventos y servicios. Se ha desarrollado con React y Vite para el frontend, empleando Tailwind CSS para el diseño. El backend está construido con Node.js, incluyendo múltiples APIs para gestionar la comunicación con la base de datos.`,
   projectCount: 2,
   projectLinks: [
     'https://ineffabile-pizza.vercel.app/',
@@ -90,7 +98,11 @@ export const skillCards = [
   icon: 'bx bx-world',
   title: 'CMS & WordPress',
   description:
-    'Tengo amplia experiencia con CMS, especialmente WordPress. He desarrollado sitios a medida, personalizado temas y plugins, y gestionado contenido optimizado para SEO. Esto me permite crear soluciones flexibles, eficientes y fáciles de mantener para diferentes tipos de clientes y proyectos.',
+    `Tengo amplia experiencia con CMS, especialmente WordPress. He desarrollado sitios a medida, personalizado temas y plugins, y gestionado contenido optimizado para SEO. Esto me permite crear soluciones flexibles, eficientes y fáciles de mantener para diferentes tipos de clientes y proyectos.
+    
+    1. Sax.es: Colaboración con la Diputación de Alicante dentro del Plan Moderniza para la migración completa del sitio web del municipio de Sax. Se trabajó con WordPress, optimizando y configurando numerosos plugins. El proyecto me permitió adquirir gran experiencia en gestión de CMS y en la adaptación de contenido institucional a un entorno digital moderno.
+    
+    2. Vysan.es: Sitio web de comercio de componentes electrónicos desarrollado en PrestaShop. Me encargué del mantenimiento general, actualización de versiones y módulos, subida de nuevos productos y modificación de precios. Este proyecto me dio experiencia práctica en la gestión de tiendas online y la operativa diaria de un e-commerce.`,
   projectCount: 2,
   projectLinks: [
     'https://www.sax.es/',
@@ -105,7 +117,9 @@ export const skillCards = [
   icon: 'bx bxl-docker',
   title: 'Contenedores & Virtualización',
   description:
-    'Conozco el uso de contenedores con Docker para crear entornos de desarrollo reproducibles y eficientes. Además, tengo experiencia en la orquestación con Kubernetes para desplegar aplicaciones escalables. Esta habilidad me permite optimizar flujos DevOps y garantizar estabilidad en producción.',
+    `Conozco el uso de contenedores con Docker para crear entornos de desarrollo reproducibles y eficientes. Además, tengo experiencia en la orquestación con Kubernetes para desplegar aplicaciones escalables. Esta habilidad me permite optimizar flujos DevOps y garantizar estabilidad en producción.
+    
+    1. Ineffabile Pizza: Proyecto de fin de grado (TFG) en el que se desarrolló una web de pedidos online para una pizzería ficticia. Utilizamos HTML, CSS y JavaScript para el frontend, Angular para el back office y Node.js como backend principal. Además, se empleó Docker para virtualizar Elasticsearch y crear un sistema de búsqueda avanzado dentro de la web.`,
   projectCount: 1,
   projectLinks: [
     'https://ineffabile-pizza.vercel.app/',
